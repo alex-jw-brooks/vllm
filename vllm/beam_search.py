e51fcc71e64f45a408d56573bd6bbf0ae82862db
alex-jw-brooks <albrooks@redhat.com>
2026-03-04 20:48:15 -0700

    online beam search mm ed

--- vllm/beam_search.py
+++ vllm/beam_search.py
@@ -3,7 +3,7 @@
 
 from dataclasses import dataclass
 
-from vllm.inputs import TokenInputs, token_inputs
+from vllm.inputs import EncoderDecoderInputs, TokenInputs, token_inputs
 from vllm.logprobs import Logprob
 from vllm.lora.request import LoRARequest
 from vllm.multimodal.inputs import MultiModalInputs, mm_inputs
@@ -17,9 +17,9 @@ class BeamSearchSequence:
     about to be returned to the user.
     """
 
-    orig_prompt: TokenInputs | MultiModalInputs
+    orig_prompt: TokenInputs | MultiModalInputs | EncoderDecoderInputs
 
-    # The tokens include the prompt.
+    # NOTE: Tokens represents decoder tokens in the encoder / decoder case
     tokens: list[int]
     logprobs: list[dict[int, Logprob]]
     lora_request: LoRARequest | None = None
@@ -31,6 +31,10 @@ class BeamSearchSequence:
     def get_prompt(self):
         prompt = self.orig_prompt
 
+        if prompt["type"] == "enc_dec":
+            return self._build_encoder_decoder_inputs(prompt)
+
+        # Handle decoder-only inputs
         prompt_text = prompt.get("prompt")
         cache_salt = prompt.get("cache_salt")
 
@@ -50,6 +54,54 @@ class BeamSearchSequence:
             cache_salt=cache_salt,
         )
 
+    def _build_encoder_decoder_inputs(self, prompt):
+        """Rebuild the encoder-decoder inputs with the current beam search
+        sequence's tokens.
+
+        NOTE: Because of the way that multimodal encoder/decoder caching is
+        currently implemented, to prevent multimodal feature recomputation at
+        decode time, we drop multimodal components from the decoder prompt.
+        This is currently needed beacuse each beam runs as a new request
+        generating one token, so num_computed_tokens=0, which is the condition
+        for calling the encoder.
+
+        TODO (alex) after caching handles this, update this accordingly.
+        """
+        dec_prompt = prompt["decoder_prompt"]
+
+        self.orig_prompt: EncoderDecoderInputs
+        num_current_tokens = len(self.tokens)
+        num_initial_tokens = len(self.orig_prompt["decoder_prompt"]["prompt_token_ids"])
+        is_decode = num_current_tokens > num_initial_tokens
+
+        if not is_decode and dec_prompt["type"] == "multimodal":
+            mm_decoder_info = {
+                "mm_kwargs": dec_prompt["mm_kwargs"],
+                "mm_hashes": dec_prompt["mm_hashes"],
+                "mm_placeholders": dec_prompt["mm_placeholders"],
+            }
+        else:
+            mm_decoder_info = {
+                "mm_kwargs": None,
+                "mm_hashes": {},
+                "mm_placeholders": {},
+            }
+
+        # Rebuild decoder prompt with updated tokens,
+        # but keep everything else the same.
+        new_dec_prompt = mm_inputs(
+            self.tokens,
+            prompt=dec_prompt.get("prompt"),
+            cache_salt=dec_prompt.get("cache_salt"),
+            **mm_decoder_info,
+        )
+
+        return EncoderDecoderInputs(
+            type="enc_dec",
+            encoder_prompt=prompt["encoder_prompt"],
+            decoder_prompt=new_dec_prompt,
+        )
+
 
 @dataclass
 class BeamSearchOutput:
@@ -64,15 +116,20 @@ class BeamSearchOutput:
 class BeamSearchInstance:
     def __init__(
         self,
-        prompt: TokenInputs | MultiModalInputs,
+        prompt: TokenInputs | MultiModalInputs | EncoderDecoderInputs,
         lora_request: LoRARequest | None = None,
         logprobs: list[dict[int, Logprob]] | None = None,
         **kwargs,
     ):
+        decoder_prompt = (
+            prompt if prompt["type"] != "enc_dec" else prompt["decoder_prompt"]
+        )
+        initial_tokens = decoder_prompt["prompt_token_ids"]
+
         self.beams: list[BeamSearchSequence] = [
             BeamSearchSequence(
                 orig_prompt=prompt,
-                tokens=prompt["prompt_token_ids"],
+                tokens=initial_tokens,
                 logprobs=[] if logprobs is None else list(logprobs),
                 lora_request=lora_request,
                 **kwargs,
